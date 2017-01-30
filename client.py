86d1b204ae7d67557b39d755cdf6d79cce68f33b
tharuni <tharuni@tharunis-MacBook-Pro.local>
2017-01-30 14:23:18 +0000

    this is the ones with threads

--- client.py
+++ client.py
@@ -1,14 +1,26 @@
 from socket import *
+from threading import Thread
 
-HOST = ''
-PORT = 8000
-s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT))
-while True:
-	message = raw_input("Your message: ")
+def sendMessage():
+	message = raw_input("Send Message")
 	s.send(message)
-	print "Awaiting reply"
-	reply = s.recv(1024)
-	print "Received", repr(reply)
 
+
+
+def receiveMessage():
+	reply = s.recv(1024)
+	print "Recieved",repr(reply)
+
+
+HOST = '10.40.131.123'
+PORT = 8000
+s = socket(AF_INET,SOCK_STREAM)
+s.connect((HOST, PORT)) # client-side, connects to a host
+
+while True:
+	message = raw_input("Your Message: ")
+	s.send(message) 
+	print "Awaiting reply"
+	reply = s.recv(1024) # 1024 is max data that can be received print "Received ", repr(reply)
+	print"Recieved",repr(reply)
 s.close()
--- server.py
+++ server.py
@@ -1,22 +1,35 @@
 #import socket
 from socket import *
+from threading import Thread
+
+
+def clientHandler():
+	conn,addr = s.accept()
+	print addr, "is Connected"
+	while 1:
+		data = conn.recv(1024)
+		if not data:
+			break
+		print "Recieved Message",repr(data)
+		s.sendall(data)
+
 
 HOST = '' #localhost
 PORT = 8000
 
-print "Program running...1"
-
 s = socket(AF_INET, SOCK_STREAM)
 s.bind((HOST,PORT))
-s.listen(1)  # how many connections it can receive at one time
-conn, addr = s.accept()  # accept the connection
-print 'Connected by' , addr  # print address of the client connected
+s.listen(5)  # how many connections it can receive at one time
+#conn, addr = s.accept()  # accept the connection
+#print 'Connected by' , addr  # print address of the client connected
+print "Program running...1"
 
-while True:
-	data = conn.recv(1024) # 1024 bytes of data it can receive
-	print "Received", repr(data)  # print data (message sent by client)
-	reply = raw_input("Reply: ")
-	conn.sendall(reply) # sends reply to every node connected
+for i in range(5):
+	Thread(target=clientHandler().start())
+# while True:
+# 	data = conn.recv(1024) # 1024 bytes of data it can receive
+# 	print "Received", repr(data)  # print data (message sent by client)
+# 	reply = raw_input("Reply: ")
+# 	conn.sendall(reply) # sends reply to every node connected
 
-print "Program running...2"
-conn.close()
+s.close()
