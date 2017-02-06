10f4dc4091bff1ba74822bc97b78ad45637fa6f2
tharuni <tharuni@tharunis-MacBook-Pro.local>
2017-02-06 11:23:44 +0000

    ability to add username,multithreading,notification of entering/exiting chatroom

--- client.py
+++ client.py
@@ -1,14 +1,46 @@
-from socket import *
+import sys 
+import socket
+import select
 
-HOST = ''
-PORT = 8000
-s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT))
-while True:
-	message = raw_input("Your message: ")
-	s.send(message)
-	print "Awaiting reply"
-	reply = s.recv(1024)
-	print "Received", repr(reply)
+def chat_client():
+	HOST = ''
+	try:
+		PORT = 8080
+	except:
+		print "not a valid port"
+		sys.exit("you have exited the chat")
 
-s.close()
+
+	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	s.settimeout(2)
+
+	try:
+		s.connect((HOST,PORT))
+	except:
+		print"impossible connection"
+		sys.exit()
+
+	print "remote connection"
+	uname = raw_input("Enter a username: ")
+	sys.stdout.write("Your message: ") ; sys.stdout.flush()
+
+	while 1:
+		socket_list = [sys.stdin, s]
+		read,write,error_sockets = select.select(socket_list,[],[])
+
+		for sock in read:
+			if sock == s:
+				data = sock.recv(4096)
+				if not data:
+					print "disconnected chat"
+					sys.exit()
+				else:
+					sys.stdout.write(data)
+					sys.stdout.write("Your message: " );sys.stdout.flush()
+			else:
+				msg = sys.stdin.readline()
+				s.send(msg)
+				sys.stdout.write("Your message: ");sys.stdout.flush()
+
+if __name__ == "__main__":
+	sys.exit(chat_client())
